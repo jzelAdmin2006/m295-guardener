67ef623eb9370a42b8c904247249111cbf139ab4
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-05-24 13:23:47 +0200

    implement authentication

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            return ['plainTextToken' => $request->user()->createToken('auth_token')->plainTextToken];
+        }
+    }
+}
